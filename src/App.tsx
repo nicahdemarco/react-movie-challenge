525aa9c1128cb7dea77b09ed2047f4201af99294
nicahdemarco <mortalfombra@gmail.com>
2022-02-09 23:23:56 -0300

    feat: Improved App and added new packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './assets/logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import { MoviesComp } from "./components/moviesComponent";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faArrowLeft, faHeart, faSearch, faStar, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { getPopularMovies } from './fetchService/fetchService';
+import { API_KEY, parsedError } from './fetchService/fetchService';
+import { IRawData } from "./types/types";
+
+library.add(fab, faArrowLeft, faHeart, faSearch, faStar, faTimes)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [appState, setAppState] = useState<IRawData>();
+
+	useEffect(() => {
+		!appState && getPopularMovies(API_KEY)
+			.then((data) => setAppState(data))
+			.catch((err) => parsedError(err))
+	}, [appState]);
+
+	return (
+		<div className="app-container">
+			{appState ?
+				<MoviesComp
+					movieResults={appState.results}
+				/> : null
+			}
+		</div>
+	);
 }
+
 
 export default App;
